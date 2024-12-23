193ef5d8d36f81a233f962c36b2c9ddce262328b
João Gabriel <joaoelsantos@gmail.com>
2024-12-23 13:13:33 -0300

    Atualização

--- Unidade01-Avaliacao02/q4.py
+++ Unidade01-Avaliacao02/q4.py
@@ -1,4 +1,7 @@
-## Jogo termo(www.term.ooo) dueto com 7 tentativas mostrar as cores
+#   João Gabriel Nóbrega Santos
+#               20242014050013
+#
+## Jogo termo(www.term.ooo) dueto com 7 tentativas
 #print("\033[;31;42mCaramba\033[0m")
 #31: Vermelho
 #32: Verde
@@ -9,20 +12,9 @@
 #37: Branco
 #0: Reseta para a cor padrão
 
-'''
-palavra1 = 
-palavra2 =
-tentativa = 1
-verde = faz parte da palavra e esta na posição correta
-amarelo = faz parte mas posição errada
-preto = não faz parte da palavra
-
-while tentativa <= 7:
-    while letra < len(palavra1):
-    while letra < len(palavra2):
-'''
-
 import random
+
+#variavel que contem as palavras validas para o jogo, fornecida pela atividade.
 palavras = (
     "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
     "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
@@ -45,53 +37,74 @@ palavras = (
     "TORRE", "TRAJE", "TREVO", "TROCO", "TRONO", "TURMA", "URUBU",
     "VALSA", "VENTO", "VERDE", "VISAO", "VINHO", "VIUVO", "ZEBRA"
 )
+# uma das regras era "somente aceite como entrada as palavras listadas no arquivo que acompanha essa atividade", -
+# -mas eu não sabia se deveria mostrar as palavras que poderiam ser usadas na tentativa. -
+# -por via das duvida eu o fiz.
 
+print("\nEssas são as possiveis palavras para se tentar: \n", palavras)
+
+#variaveis que escolhem uma palavra do pool para cada variavel.
 palavra1 = random.choice(palavras)
 palavra2 = random.choice(palavras)
-print(palavra1)
-print(palavra2)
+
+#print(palavra1)
+#print(palavra2)
+
+#variavel que conterá se a palavra ainda não foi advinhada ainda.
 tentando_palavra1 = True
 tentando_palavra2 = True
-"""
-a = "VIUVO"
-print(f"\033[1;37;42m {a}", end="-")
-print("")
-print(f"arroz{palavra1[0]}")
-"""
 
+#variavel contadora de tentativas.
 tentativa = 1
-palavra_tentativa = input("Digite sua tentativa: ")
-#if a in palavras:
-#    print("")
+
 
 #erro: quando a primeira letra da tentativa é igual ou existe na palavra todas as outras letras ficam amarelas
 #achei o motivo, só precisei ler a linha 75 e 86, no lugar de (letra) estava (0)
 while tentativa <= 7:
-    #for ind in palavra
-    letra = 0
-    while tentando_palavra1 and letra < len(palavra1):
-        if palavra_tentativa[letra] == palavra1[letra]:
-            print(f"\033[1;37;42m{palavra_tentativa[letra]}\033[m", end="-")
-        elif palavra_tentativa[letra] in palavra1:
-            print(f"\033[1;37;43m{palavra_tentativa[letra]}\033[m", end="-")
-        else: print(f"\033[1;37;41m{palavra_tentativa[letra]}\033[m", end="-")
-        if letra == len(palavra1) - 1 and  palavra_tentativa == palavra1:
-            tentando_palavra1 = False
-        letra += 1
-    letra = 0
-    print("")
-    while tentando_palavra2 and letra < len(palavra2):
-        if palavra_tentativa[letra] == palavra2[letra]:
-            print(f"\033[1;37;42m{palavra_tentativa[letra]}\033[m", end="-")
-        elif palavra_tentativa[letra] in palavra2:
-            print(f"\033[1;37;43m{palavra_tentativa[letra]}\033[m", end="-")
-        else: print(f"\033[1;37;41m{palavra_tentativa[letra]}\033[m", end="-")
-        if letra == len(palavra2) - 1 and palavra_tentativa == palavra2:
-            tentando_palavra2 = False
-        letra += 1
-    print("")
+    #condicional que pergunta ao usuario sua proxima tentativa se uma das palavras ainda não foi advinhada. -
+    # - alem de mostrar uma mensagem e parar o laço de repetição se o usuario advinhou as duas palavras.
     if tentando_palavra1 or tentando_palavra2:
-        palavra_tentativa = input("Digite sua tentativa: ")
+            palavra_tentativa = input("\nDigite sua tentativa, toda em caixa alta(Capslock) e parte das palavras possiveis: ")
     else:
+        print("Você acertou as duas palavras. Parabens!")
         tentativa += 10
-    tentativa += 1
+    
+    if palavra_tentativa in palavras:
+        #variavel contadora do indice da letra
+        letra = 0
+        #laço para verificar cada letra se se ela esta no lugar correto, se apenas exite na primeira palavra ou se -
+        #- nem existe na primeira palavra e mostrar o resultado de acordo com as regras do jogo termo.
+        while tentando_palavra1 and letra < len(palavra1):
+            if palavra_tentativa[letra] == palavra1[letra]:
+                print(f"\033[1;37;42m{palavra_tentativa[letra]}\033[m", end="-")
+            elif palavra_tentativa[letra] in palavra1:
+                print(f"\033[1;37;43m{palavra_tentativa[letra]}\033[m", end="-")
+            else: print(f"\033[1;37;41m{palavra_tentativa[letra]}\033[m", end="-")
+            if letra == len(palavra1) - 1 and  palavra_tentativa == palavra1:
+                tentando_palavra1 = False
+            letra += 1
+
+        #variavel contadora do indice da letra
+        letra = 0
+        
+        #função para pular uma linha e mostrar o resultado da segunda palavra nessa proxima linha.
+        print("")
+        
+        #laço para verificar cada letra se se ela esta no lugar correto, se apenas exite na segunda palavra ou se- 
+        # -nem existe na segunda palavra e mostrar o resultado de acordo com as regras do jogo termo.
+        while tentando_palavra2 and letra < len(palavra2):
+            if palavra_tentativa[letra] == palavra2[letra]:
+                print(f"\033[1;37;42m{palavra_tentativa[letra]}\033[m", end="-")
+            elif palavra_tentativa[letra] in palavra2:
+                print(f"\033[1;37;43m{palavra_tentativa[letra]}\033[m", end="-")
+            else: print(f"\033[1;37;41m{palavra_tentativa[letra]}\033[m", end="-")
+            if letra == len(palavra2) - 1 and palavra_tentativa == palavra2:
+                tentando_palavra2 = False
+            letra += 1
+        
+        #função para pular uma linha e mostrar o resultado da primeira palavra e as proximas mensagens nessa-
+        # -proxima linha.
+        print("")
+        tentativa += 1
+    else:
+        print("Palavra invalida. Por favor digite uma tentativa valida")
--- Unidade01-Avaliacao02/q5.py
+++ Unidade01-Avaliacao02/q5.py
@@ -17,12 +17,6 @@ dia_anterior = 27
 mes_anterior = 4
 ano_anterior = 1968
 
-#data_anterior = 27-04-1968
-#27/04/1968
-#dia_anterior = data_anterior.day
-#mes_anterior = data_anterior.month
-#ano_anterior = data_anterior.year
-
 #variaveis dos meses de um ano e quantos dias cada um tem
 mes1 = 31
 mes2 = 28
@@ -43,17 +37,15 @@ dias_totais = 0
 #condicional que atualiza quantos dias o mês de fevereiro tem se o ano anterior for bisexto
 if ano_anterior % 4 == 0 or ano_anterior % 400 == 0 and ano_anterior % 100 != 0:
     mes2 = 29
-    print('ok1') #usado para teste
+    #print('ok1') #usado para teste
 
 #condicionais que adicionam os dias até o fim do mes da data informada ao total de dias
 if dia_anterior <= 31 and mes_anterior == 1 or mes_anterior == 3 or mes_anterior == 5 or mes_anterior == 7 or mes_anterior == 8 or mes_anterior == 10 or mes_anterior == 12:
     dias_totais += 31 - dia_anterior
-#else:
     
 if dia_anterior <= 30 and mes_anterior == 4 or mes_anterior == 6 or mes_anterior == 9 or mes_anterior == 11:
     dias_totais += (30 - dia_anterior)
-    print('ok2') #usado para teste
-#else:
+    #print('ok2') #usado para teste
 
 #condicionais que adicionam os dias dos meses restantes do ano ao total de dias 
 if dia_anterior <= 31 and mes_anterior == 1:
@@ -67,7 +59,7 @@ if dia_anterior <= 31 and mes_anterior == 3:
 
 if dia_anterior <= 30 and mes_anterior == 4:
     dias_totais += mes5 + mes6 + mes7 + mes8 + mes9 + mes10 + mes11 + mes12
-    print('ok3') #usado para teste
+    #print('ok3') #usado para teste
 
 if dia_anterior <= 31 and mes_anterior == 5:
     dias_totais += mes6 + mes7 + mes8 + mes9 + mes10 + mes11 + mes12
@@ -94,26 +86,9 @@ if dia_anterior <= 30 and mes_anterior == 11:
 if dia_anterior <= 31 and mes_anterior == 12:
     dias_totais += 0
 
-'''
-if dia_anterior <= 30 and mes_anterior == 4:
-    dias_totais += mes5 + mes6 + mes7 + mes8 + mes9 + mes10 + mes11 + mes12
-    print('ok3')
-
-if dia_anterior <= 30 and mes_anterior == 6:
-    dias_totais += mes7 + mes8 + mes9 + mes10 + mes11 + mes12
-
-if dia_anterior <= 30 and mes_anterior == 9:
-    dias_totais += mes10 + mes11 + mes12
-
-if dia_anterior <= 30 and mes_anterior == 11:
-    dias_totais += mes12
-
-if dia_anterior <= 29 and mes_anterior == 2:
-    dias_totais += mes3 + mes4 + mes5 + mes6 + mes7 + mes8 + mes9 + mes10 + mes11 + mes12
-'''
 #laço que adiciona os dias de cada ano passado até 1 ano anterior ao de hoje e variavel contadora para o laço
 anterior = ano_anterior + 1
-#bisexto = 0 #não sei pq fiz essa variavel
+
 while anterior < ano:
     if anterior % 400 == 0 or anterior % 4 == 0 and anterior % 100 != 0:
         dias_totais += 366
@@ -130,12 +105,10 @@ else: mes2 = 28
 #condicionais que adicionam os dias do inicio do mes até data de hoje ao total de dias
 if dia <= 31 and mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
     dias_totais += dia
-    print('ok4') #usado para teste
-#else:
+    #print('ok4') #usado para teste
     
 if dia <= 30 and mes == 4 or mes == 6 or mes == 9 or mes == 11:
     dias_totais += dia
-#else:
 
 ##condicional inutil, estava copiando/colando e atualizando as condicionais e fiz esse no automatico. Resolvi deixar, efeito na performance é ignoravel
 if dia <= 31 and mes == 1:
@@ -174,25 +147,7 @@ if dia <= 30 and mes == 11:
 
 if dia <= 31 and mes == 12:
     dias_totais += mes1 + mes2 + mes3 + mes4 + mes5 + mes6 + mes7 + mes8 + mes9 + mes10 + mes11
-    print('ok5') #usado para teste
-'''
-if dia <= 30 and mes == 4:
-    dias_totais += mes1 + mes2 + mes3 
-
-if dia <= 30 and mes == 6:
-    dias_totais += mes1 + mes2 + mes3 + mes4 + mes5 
-
-if dia <= 30 and mes == 9:
-    dias_totais += mes1 + mes2 + mes3 + mes4 + mes5 + mes6 + mes7 + mes8 
-
-if dia <= 30 and mes == 11:
-    dias_totais += mes1 + mes2 + mes3 + mes4 + mes5 + mes6 + mes7 + mes8 + mes9 + mes10
-
-if dia <= 29 and mes == 2:
-    dias_totais += mes1
-'''
-
-#print(bisexto) #ainda n lembro porque eu fiz isso, talvez pensei que precisaria
+    #print('ok5') #usado para teste
 
 #variaveis e condicionais usadas para descobrir quantos sabados houveram da data informada até hoje
 sabados = dias_totais // 7
@@ -207,16 +162,6 @@ if resto == 0:
 #funções que mostra o resultado do calculo
 print("Houveram ", dias_totais, " dias desde ", dia_anterior, "-", mes_anterior,"-",  ano_anterior, "até hoje")
 print("Houveram ", sabados, " sabados desde ", dia_anterior, "-", mes_anterior,"-",  ano_anterior, "até hoje")
-
-#Funções usadas para teste
-'''
-print(resto)
-print(date)
-print(dia)
-print(mes)
-print(ano)
-print(type(date))
-'''
 
 ### Usei para verificar se o resultado estava correto, fi de madrugada e o chatGPT não estava fazendo "calculos complexos" segundo o mesmo
 ## codigo para verificar os dias
@@ -236,10 +181,10 @@ from datetime import datetime, timedelta
 # Defina as datas de início e fim
 data_inicial = datetime(1968, 4, 27)
 data_final = datetime.now()
-'''
+
 
 ## codigo para verificar os sabados
-'''
+
 # Função para contar os sábados
 def contar_sabados(data_inicial, data_final):
     count = 0
